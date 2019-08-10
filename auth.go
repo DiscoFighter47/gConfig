6b9b07324f523153fc598cb31263a517b2c436c0
Zahid Al Tair <mdzahidh119@gmail.com>
2019-08-11 02:30:53 +0600

    added auth config

---
+++ auth.go
@@ -0,0 +1,48 @@
+package gconfig
+
+import (
+	"log"
+	"sync"
+	"time"
+
+	gson "github.com/DiscoFighter47/gSON"
+	"github.com/spf13/viper"
+)
+
+// AuthCfg ..
+type AuthCfg struct {
+	Secret             string
+	TokenExpireTimeout time.Duration
+}
+
+func (auth *AuthCfg) validate() {
+	errV := gson.ValidationError{}
+	if auth.Secret == "" {
+		errV.Add("auth.secret", "missing")
+	}
+	if auth.TokenExpireTimeout == 0 {
+		errV.Add("auth.token_expire_timeout", "missing")
+	}
+	if len(errV) > 0 {
+		log.Fatal("Auth Configuration Error:", errV)
+	}
+}
+
+var auth *AuthCfg
+var authOnce sync.Once
+
+func loadAuth() {
+	auth = &AuthCfg{
+		Secret:             viper.GetString("auth.secret"),
+		TokenExpireTimeout: viper.GetDuration("auth.token_expire_timeout") * time.Minute,
+	}
+}
+
+// Auth ...
+func Auth() *AuthCfg {
+	authOnce.Do(func() {
+		loadAuth()
+		auth.validate()
+	})
+	return auth
+}
